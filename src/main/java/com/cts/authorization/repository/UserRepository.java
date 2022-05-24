a0bd433f41742f7e122229a3755d9cad686d3eca
srikant2411 <kvenkata.srikantbabu@cognizant.com>
2022-05-24 08:21:42 +0530

    first commit

---
+++ src/main/java/com/cts/authorization/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.cts.authorization.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cts.authorization.model.User;
+
+/**
+ * Repository for User
+ * 
+ *  @author Srikant Babu
+ *  @author 894137
+ *
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
